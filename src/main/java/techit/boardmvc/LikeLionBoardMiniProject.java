a41fe97901ce0be64410e37d97002fcd401adbd6
CodeJesterND <dev.hyoseung@gmail.com>
2024-07-10 18:20:20 +0900

    Project name modify

--- src/main/java/techit/boardmvc/LikeLionBoardMiniProject.java
+++ src/main/java/techit/boardmvc/LikeLionBoardMiniProject.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BoardMvcApplication {
+public class LikeLionBoardMiniProject {
 
     public static void main(String[] args) {
-        SpringApplication.run(BoardMvcApplication.class, args);
+        SpringApplication.run(LikeLionBoardMiniProject.class, args);
     }
 }
